2a574c1938517f2e3fc2d38805a337adf8d335f8
Kent Daleng <kent.stefan.daleng@nav.no>
2020-07-09 22:38:00 +0200

    Fix syntax errors, add gitignore and a setup.cfg file

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="naislinter",
     version="1.0",
     license="MIT",
-    url="https://github.com/chinatsu/naislinter"
-    download_url="https://github.com/chinatsu/naislinter/archive/v1.0.tar.gz"
+    url="https://github.com/chinatsu/naislinter",
+    download_url="https://github.com/chinatsu/naislinter/archive/v1.0.tar.gz",
     description="Lints naiserator.yaml files",
     author="Kent Daleng",
     author_email="kent.stefan.daleng@nav.no",
